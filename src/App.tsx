d3d6bbbe9b2a0b88dfa14c3f0a261df1cdcd88e8
matheuslesbao <matheuss.lesbao@gmail.com>
2022-07-06 20:15:37 -0300

    feat: inclusa do tailwindcss e inicio da estrutura html

--- src/App.tsx
+++ src/App.tsx
@@ -7,37 +7,21 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+      <header>
+        <h1>Tente Nao Rir</h1>
       </header>
+      <main>
+        <div id="players">
+          <h2>Player 1</h2>
+          <h2>Player 2</h2>
+          <h2>Player 3</h2>
+        </div>
+        <div id="scores">
+          <div>0</div>
+          <div>1</div>
+          <div>2</div>
+        </div>
+      </main>
     </div>
   )
 }
